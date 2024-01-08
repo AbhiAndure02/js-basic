39f7bc5788f31052ba08c6891fd43b00d713a281
Abhishek  Andure <91086876+AbhiAndure02@users.noreply.github.com>
2024-01-08 16:59:05 +0530

    Update coding.js

--- part 01/coding.js
+++ part 01/coding.js
@@ -1,3 +1,82 @@
+//variable 
+
+// var// vaparat nahi
+// let 
+// const
+
+// {
+//     let  s = "not you type"
+
+// }
+// console.log(s)
+// let a = 44;
+// const b =54;
+// console.log(a)
+// console.log(b)
+
+// a = 78;
+// console.log(a)
+
+// b = 77;
+// console.log(b)
+
+
+
+
+
+// var name ="vishwas"
+
+
+
+// {
+//    var name= "abhishek"
+// }
+
+// console.log(name)
+
+// String 
+// primitive  = String, boolean, number
+// non-primitive = object, array
+// let r = "maruti suzuki";
+// console.log(r.)
+// console.log(a)
+// let a = 67;
+// console.log(typeof a)
+// //let b = a.toString()
+// console.log(typeof a.toString())
+
+// number 
+// let s ="2"
+
+// console.log(s.toFixed())
+// console.log(s.toPrecision())
+// // console.log(s.toLocaleString('en-In'))
+// let a =s.valueOf()
+// console.log(typeof a)
+
+// boolean 
+
+// let isOdd = false;/true
+// console.log(typeof isOdd)
+// const ss = document.getElementById("ss")
+// console.log(ss)
+
+// Array
+
+// let arr = [2,4,6,64,3,2,3]
+// console.log(arr)
+
+//reverse 
+// arr.push(3)
+// console.log(arr)
+// arr.unshift(1)
+// console.log(arr)
+// console.log(arr.sort((a,b) => a-b))
+
+
+
+
+
 // const cod = ['js','java','c','c++']
 // const values = cod.forEach((item)=>{
 //     console.log(item)
